f3c73d4705210e3804ef8c82c7b691078d5c4102
dbenmir <diegobenaventemira@gmail.com>
2018-11-12 18:10:14 +0100

    Update Aplicacion.java

--- src/main/java/com/inigoserrano/openshift/aplicacion/Aplicacion.java
+++ src/main/java/com/inigoserrano/openshift/aplicacion/Aplicacion.java
@@ -25,7 +25,7 @@ public class Aplicacion extends HttpServlet {
 	@Override
     protected void doGet(final HttpServletRequest request, final HttpServletResponse response) throws ServletException, IOException {
         System.out.println("Salida GET modificada con webhook ReNuevo");
-        response.getWriter().println("Salida GET version master webhook Nuevooooo");  
+        response.getWriter().println("Salida GET version master webhook ReNuevooooo");  
         response.getWriter().flush();
     }
 
